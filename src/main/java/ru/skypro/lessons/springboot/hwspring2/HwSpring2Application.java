2d212b36b71c716f5abca82a6e4834f26938c35d
Елизавета Носкова <wliza68@gmail.com>
2023-07-02 10:18:06 +0300

    first commit

---
+++ src/main/java/ru/skypro/lessons/springboot/hwspring2/HwSpring2Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.hwspring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSpring2Application.class, args);
+    }
+
+}
