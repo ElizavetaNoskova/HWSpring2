b0dd8dfc5486563cf8b5c2aad66fc32e8465531f
Елизавета Носкова <wliza68@gmail.com>
2023-07-08 16:20:28 +0300

    security

---
+++ src/test/java/ru/skypro/lessons/springboot/hwspring2/HwSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.hwspring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpring2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
