62c6689faaa8a064b7c91668dbd580521c4fe745
Елизавета Носкова <wliza68@gmail.com>
2023-07-08 16:06:01 +0300

    first commit

--- src/main/java/ru/skypro/lessons/springboot/hwspring2/controler/EmployeeControler.java
+++ src/main/java/ru/skypro/lessons/springboot/hwspring2/controler/EmployeeControler.java
@@ -1,79 +1,57 @@
 package ru.skypro.lessons.springboot.hwspring2.controler;
-
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.lessons.springboot.hwspring2.model.Employee;
 import ru.skypro.lessons.springboot.hwspring2.DTO.EmployeeDTO;
 import ru.skypro.lessons.springboot.hwspring2.service.EmployeeService;
-
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-
 @RestController
 @RequestMapping("/employee")
 public class EmployeeControler {
     private final EmployeeService employeeService;
-
     public EmployeeControler(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
-
     @GetMapping("/salary/all")
     public List<EmployeeDTO> showAll() {
         return employeeService.getAllEmployees();
     }
-
     @GetMapping("/salary/sum")
     public Integer showSum() {
         return employeeService.getSalarySum();
     }
-
     @GetMapping("/salary/min")
     public Optional<Integer> showMin() {
         return employeeService.getMinSalary();
     }
-
     @GetMapping("/salary/withHighestSalary")
     public Optional<Integer> showMax() {
         return employeeService.getMaxSalary();
     }
-
     @GetMapping("/salary/higherThenAvg")
     public List<EmployeeDTO> showHigherThenAvg() {
         return employeeService.getAllEmployeesWithSalaryHigherThenAvg();
     }
-
     @PostMapping("/add")
     public void addEmployee(@RequestBody Employee employee) {
         employeeService.addEmployee(employee);
     }
-
     @PutMapping("/update")
     public void updateEmployee(@RequestBody Employee employee) {
         employeeService.updateEmployee(employee);
     }
-
     @GetMapping("/getBy{id}")
     public List<EmployeeDTO> getEmployeeById(@PathVariable Integer id) {
         return employeeService.getEmployeeById(id);
     }
-
     @DeleteMapping("/deleteBy{id}")
     public void deleteEmployeeById(@PathVariable Integer id) {
         employeeService.deleteEmployeeById(id);
     }
-
     @GetMapping("/salary/higher")
     public List<EmployeeDTO> getAllEmployeesWithSalaryHigherThan(@RequestParam("salary") int salary) {
         return employeeService.getAllEmployeesWithSalaryHigherThan(salary);
     }
-
     @GetMapping("/position")
     public List<EmployeeDTO> getAllEmployeesWithMatchingPosition(@RequestParam("position") String position) {
 
@@ -87,13 +65,8 @@ public class EmployeeControler {
     }
 
     @GetMapping("/page")
-    public List<EmployeeDTO> getEmployeesInPageFormat(@RequestParam(required = false, defaultValue = "0") int page) {
+    public List<EmployeeDTO> getEmployeesInPageFormat(@RequestParam(required = false,defaultValue = "0") int page) {
 
         return employeeService.getEmployeesInPageFormat(page);
-    }
-
-    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void upload(@RequestParam("file") MultipartFile file) throws IOException {
-        employeeService.saveEmployeeFromJson(file);
     }
 }
