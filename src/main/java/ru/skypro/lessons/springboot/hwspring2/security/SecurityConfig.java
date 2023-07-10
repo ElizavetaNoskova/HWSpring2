7a1c280bc63239275e242ef0934d4108e9f48e01
Елизавета Носкова <wliza68@gmail.com>
2023-07-10 17:48:44 +0300

    исправлено

--- src/main/java/ru/skypro/lessons/springboot/hwspring2/security/SecurityConfig.java
+++ src/main/java/ru/skypro/lessons/springboot/hwspring2/security/SecurityConfig.java
@@ -1,14 +1,17 @@
 package ru.skypro.lessons.springboot.hwspring2.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -34,6 +37,19 @@ public class SecurityConfig {
         jdbcUserDetailsManager.setAuthenticationManager(authenticationManager);
 
         return jdbcUserDetailsManager;
+    }
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public DaoAuthenticationProvider authProvider(PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
     }
 
     @Bean
@@ -65,7 +81,7 @@ public class SecurityConfig {
                     .logout().logoutUrl("/logout");
 
         } catch (Exception e) {
-            throw new RuntimeException();
+            throw new RuntimeException(e);
         }
     }
 }
