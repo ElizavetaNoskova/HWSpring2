ceb69a614541ea5268bb2e92d30305f211fd7f42
Елизавета Носкова <wliza68@gmail.com>
2023-07-14 16:50:52 +0300

    liquibase

--- src/main/java/ru/skypro/lessons/springboot/hwspring2/controler/InfoControler.java
+++ src/main/java/ru/skypro/lessons/springboot/hwspring2/controler/InfoControler.java
@@ -2,14 +2,18 @@ package ru.skypro.lessons.springboot.hwspring2.controler;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/info")
 public class InfoControler {
+
     @Value("${app.env}")
-    String env;
+    String appInfo;
+
     @GetMapping("/appInfo")
-    public String appInfo(){
-        return env;
-    };
+    public String getAppInfo() {
+        return appInfo;
+    }
 }
