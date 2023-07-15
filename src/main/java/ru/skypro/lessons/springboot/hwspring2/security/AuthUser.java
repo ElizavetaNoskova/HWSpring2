cb6b2ec7b51dd7b06657357351ea6e4d207efae5
Елизавета Носкова <wliza68@gmail.com>
2023-07-15 16:23:41 +0300

    исправление ошибок

--- src/main/java/ru/skypro/lessons/springboot/hwspring2/security/AuthUser.java
+++ src/main/java/ru/skypro/lessons/springboot/hwspring2/security/AuthUser.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Setter
 public class AuthUser {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
