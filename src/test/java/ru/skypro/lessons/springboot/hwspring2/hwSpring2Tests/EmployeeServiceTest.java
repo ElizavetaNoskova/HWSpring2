c1cb5fd79fb8cd4c147bf92386e9d85d95716feb
Елизавета Носкова <wliza68@gmail.com>
2023-08-17 11:05:11 +0300

    перенесла пакет

--- src/test/java/ru/skypro/lessons/springboot/hwspring2/hwSpring2Tests/EmployeeServiceTest.java
+++ src/test/java/ru/skypro/lessons/springboot/hwspring2/hwSpring2Tests/EmployeeServiceTest.java
@@ -1,4 +1,4 @@
-package ru.skypro.lessons.springboot.hwspring2.HWSpring2Tests;
+package ru.skypro.lessons.springboot.hwspring2.hwSpring2Tests;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
--- src/test/java/ru/skypro/lessons/springboot/hwspring2/hwSpring2Tests/ReportServiceTest.java
+++ src/test/java/ru/skypro/lessons/springboot/hwspring2/hwSpring2Tests/ReportServiceTest.java
@@ -1,37 +1,20 @@
-package ru.skypro.lessons.springboot.hwspring2.HWSpring2Tests;
+package ru.skypro.lessons.springboot.hwspring2.hwSpring2Tests;
 
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mock.web.MockMultipartFile;
-import ru.skypro.lessons.springboot.hwspring2.DTO.EmployeeDTO;
 import ru.skypro.lessons.springboot.hwspring2.DTO.ReportDTO;
-import ru.skypro.lessons.springboot.hwspring2.model.Employee;
 import ru.skypro.lessons.springboot.hwspring2.model.Report;
-import ru.skypro.lessons.springboot.hwspring2.repository.EmployeeRepository;
 import ru.skypro.lessons.springboot.hwspring2.repository.ReportRepository;
-import ru.skypro.lessons.springboot.hwspring2.service.EmployeeMapper;
-import ru.skypro.lessons.springboot.hwspring2.service.EmployeeService;
-import ru.skypro.lessons.springboot.hwspring2.service.EmployeeServiceImpl;
 import ru.skypro.lessons.springboot.hwspring2.service.ReportServiceImpl;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -43,7 +26,7 @@ public class ReportServiceTest {
     private ReportServiceImpl reportService;
 
     @Test
-    void ReportTest() throws IOException {
+    void reportTest() throws IOException {
         Integer position = 1;
         Long countEmployees = 1L;
         Integer maxSalary = 100;
